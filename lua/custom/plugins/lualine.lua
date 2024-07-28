f367fe9a7b43236ee21f1f7215d1b83033080b36
Bill Fischer <BillFischer510@gmail.com>
2024-07-28 05:51:33 -0700

    Add lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,6 +1,26 @@
 return {
     'nvim-lualine/lualine.nvim',
+    event = 'VeryLazy',
+    init = function()
+        vim.g.lualine_laststatus = vim.o.laststatus
+        if vim.fn.argc(-1) > 0 then
+            vim.o.statusline = ' '
+        else
+            vim.o.laststatus = 0
+        end
+    end,
     opts = {
         theme = 'dracula',
+        icons_enabled = true,
+        disabled_filetypes = {
+            statusline = { 'neo-tree', 'toggleterm' },
+            winbar = {},
+        },
+        ignore_focus = {
+            'neo-tree',
+            'toggleterm',
+            'aerial',
+        },
+        extensions = { 'neo-tree', 'lazy', 'fzf', 'trouble' },
     },
 }
