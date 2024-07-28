86cf13b1390e9573b31243a73ef7b82624da1289
Bill Fischer <BillFischer510@gmail.com>
2024-07-28 05:52:27 -0700

    Enable a floating window with the git status for neo-tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -2,24 +2,30 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  cmd = 'Neotree',
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal' },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
+    'nvim-neo-tree/neo-tree.nvim',
+    version = '*',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+        'MunifTanjim/nui.nvim',
     },
-  },
+    cmd = 'Neotree',
+    keys = {
+        { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal' },
+        { '<leader>gs', ':Neotree reveal git_status float<CR>', desc = '[G]it [S]tatus preview' },
+    },
+    opts = {
+        filesystem = {
+            window = {
+                mappings = {
+                    ['\\'] = 'close_window',
+                },
+            },
+        },
+        open_files_do_not_replace_types = { 'terminal', 'trouble', 'qf' },
+        hijack_netrw_behavior = 'open_default',
+        follow_current_file = {
+            enabled = true,
+        },
+    },
 }
